199165db7a3be10e223bc00556c3cfadc5bafa64
Washington Yandun <washington.yandun06@gmail.com>
2022-09-08 23:43:49 -0500

    improve models and validations

--- main.py
+++ main.py
@@ -1,18 +1,51 @@
-from typing import List, Optional
-from unittest import result
-from pydantic import BaseModel
-from fastapi import FastAPI
+import email
+from enum import Enum
 from fastapi import Body, Query, Path
+from fastapi import FastAPI
+from pydantic import Field
+from pydantic import EmailStr, PaymentCardNumber, FilePath
+from pydantic import BaseModel
+from typing import List, Optional
+
 
 app = FastAPI()
 
 # Models
 
 
+class EyeColor(Enum):
+    brown = "brown"
+    blue = "blue"
+    green = "green"
+
+
 class Person(BaseModel):
-    name: str
-    age: int
-    is_married: Optional[bool] = None
+    name: str = Field(
+        ...,
+        example="Wmym",
+        min_length=2,
+        max_length=10)
+    age: int = Field(
+        ...,
+        example=18,
+        ge=18,
+        le=100
+    )
+    is_married: Optional[bool] = Field(
+        default=False
+    )
+    eyes_color: Optional[EyeColor] = Field(
+        default="brown",
+        example="brown"
+    )
+    email: Optional[EmailStr] = Field(
+        default=None,
+        example="***@***.com"
+    )
+    card_number: Optional[PaymentCardNumber] = Field(
+        default=None,
+        example="1234-1234-1234-1234"
+    )
 
 
 class Location(BaseModel):
@@ -20,14 +53,14 @@ class Location(BaseModel):
     country: str
 
 
-@app.get("/")
+@ app.get("/")
 def home():
     return {"world": "mundo"}
 
 # python logic is still useful
 
 
-@app.get("/tweet/{tweet_id}")
+@ app.get("/tweet/{tweet_id}")
 def tweet(tweet_id: int):
     if tweet_id % 2 == 0:
         return {"tweet_id": "divisible by 2"}
@@ -36,14 +69,14 @@ def tweet(tweet_id: int):
 # request and response Body
 
 
-@app.post("/person/new")
+@ app.post("/person/new")
 def new_person(person: Person = Body(...)):
     return {person}
 
 # Query parameters
 
 
-@app.get("/person/details")
+@ app.get("/person/details")
 def person_details(
     name: Optional[str] = Query(
         None,
@@ -62,7 +95,7 @@ def person_details(
 
 # path parameters
 
-@app.get("/person/details/{id}")
+@ app.get("/person/details/{id}")
 def person_details(
     id: int = Path(
         ...,
@@ -77,8 +110,33 @@ def person_details(
 
 # Validiations request body
 
-@app.put("/person/update/{id}")
+@ app.post("/person/new_post/{id}")
 def update_person(
+    id: int = Path(
+        ...,
+        gt=0,
+        le=100,
+        title="ID",
+        description="ID of the person"
+    ),
+    file: FilePath = Body(
+        ...,
+        title="File",
+        description="File to upload"
+    ),
+    person: Person = Body(
+        ...,
+        title="Person",
+        description="Person to update"
+    )
+
+):
+    return {id: person}
+
+
+# Validations request body with pydantic
+@ app.put("/person/update/data/{id}")
+def update_person_data(
     id: int = Path(
         ...,
         gt=0,
@@ -99,4 +157,4 @@ def update_person(
 ):
     result = dict(person)
     result.update(dict(location))
-    return result
+    return {id: result}
